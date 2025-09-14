a7e6d1743649d6a519557f962ff7109c9b6e60c6
Akash-YS05 <akash.pan05@gmail.com>
2025-09-14 20:13:16 +0530

    devnet connection

--- app/utils/anchor.ts
+++ app/utils/anchor.ts
@@ -1,5 +1,5 @@
 import { AnchorProvider, Program, Idl } from "@coral-xyz/anchor";
-import type { Wallet } from "@coral-xyz/anchor/dist/cjs/provider"; // ✅ browser Wallet type
+import type { Wallet } from "@coral-xyz/anchor/dist/cjs/provider"; 
 import { Connection, PublicKey } from "@solana/web3.js";
 import idl from "../idl/keychain.json";
 
@@ -8,9 +8,8 @@ const programID = new PublicKey("TXHddDGCYaoQqPmQatT7xYAWPfrSaVo5FosrxaTcncF");
 export function getProvider(walletAdapter: any): AnchorProvider {
   if (!walletAdapter?.publicKey) throw new Error("Wallet not connected");
 
-  const connection = new Connection("http://127.0.0.1:8899", "confirmed");
+  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
-  // Cast the adapter into the Wallet interface
   const wallet: Wallet = {
     publicKey: walletAdapter.publicKey,
     signTransaction: walletAdapter.signTransaction,
